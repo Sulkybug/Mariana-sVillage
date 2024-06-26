e9175b6fea3abeb235dfab5d87573d2020095058
Sulkybug <feang_c0@hotmail.com>
2024-06-25 21:53:51 -0500

    replacing react dom router with hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./App.tsx";
 import AboutUsPage from "./Pages/AboutUsPage.tsx";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/about-us",
-      element: <AboutUsPage />,
-    },
-  ],
-  { basename: "/Mariana-sVillage" }
-);
-
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about-us" element={<AboutUsPage />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
